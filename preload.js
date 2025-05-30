9493b6abe36cce64616fc196ee8b1f2676e34a25
Solmn <solomonmolla10@gmail.com>
2025-05-30 09:06:56 -0500

    update

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getData: (key) => ipcRenderer.invoke('get-data', key),
-  setData: (key, value) => ipcRenderer.invoke('set-data', key, value),
-  clearData: (key) => ipcRenderer.invoke('clear-data', key),
-  selectFile: () => ipcRenderer.invoke('select-file')
-});
